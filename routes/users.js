6d12476de7b11549721adcf75a93c774ce9de30f
Felix Mutua <felixmutua01@gmail.com>
2020-06-05 17:44:19 +0300

    Use DRY principle

--- routes/users.js
+++ routes/users.js
@@ -5,16 +5,33 @@ const usersData = "./data/users.json";
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  fs.readFile(usersData, "utf8", (err, data) => {
-    if (err) {
-      throw err;
-    }
+router.get('/', function (req, res, next) {
+    fs.readFile(usersData, "utf8", (err, data) => {
+        if (err) {
+            throw err;
+        }
 
-    res.send(JSON.parse(data));
-  });
+        res.send(JSON.parse(data));
+    });
 });
-
+/* GET user. */
+router.get('/:id', function (req, res, next) {
+    let usersArray = [];
+    fs.readFile(usersData, "utf8", (err, data) => {
+        if (err) {
+            throw err;
+        }
+        usersArray = JSON.parse(data);
+    });
+    usersArray.forEach(function (user) {
+        if (user.id === req.params.id) {
+            console.log(user);
+            res.send(user)
+        } else {
+            res.send("User Not Found");
+        }
+    });
+});
 
 
 module.exports = router;
